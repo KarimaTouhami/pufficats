ecf6a95600c968643c8fa3b4691150065d30f622
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2024-05-15 21:57:44 +0100

    updated

---
+++ src/App.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div id="wrapper">
+        <div id="main">
+          <div id="class">
+          <section id="home-section" className="active">
+    <div id="container01" className="container columns full screen">
+      <div className="wrapper">
+        <div className="inner">
+          <div>
+            <div id="image01" className="image full">
+              <span className="frame">
+                <img src="https://pufficats.carrd.co/assets/images/image01.jpg?v=06f2ac22" alt="" />
+              </span>
+            </div>
+          </div>
+          <div>
+            <h2 id="text01">PUFFI<em>CATS.</em></h2>
+            <ul id="links03" className="style1 links">
+              <li className="n01"><a href="#i">guide.</a></li>
+              <li className="n02"><a href="#ii">roster.</a></li>
+              <li className="n03"><a href="#iii">connections.</a></li>
+            </ul>
+            <ul id="icons01" className="icons">
+              <li>
+                <a className="n01" href="https://www.instagram.com/puffic4ts/">
+                  <svg aria-labelledby="icon-title1">
+                    <title id="icon-title1">Instagram</title>
+                    <use xlinkHref="#icon-85976685de3e4af37529a1ce5d57d2a7"></use>
+                  </svg>
+                  <span className="label">Instagram</span>
+                </a>
+              </li>
+              <li>
+                <a className="n02" href="https://www.wattpad.com/user/pufficats">
+                  <svg aria-labelledby="icon-title2">
+                    <title id="icon-title2">Wattpad</title>
+                    <use xlinkHref="#icon-d95047674115cba289c0870d1571ee59"></use>
+                  </svg>
+                  <span className="label">Wattpad</span>
+                </a>
+              </li>
+              <li>
+                <a className="n03" href="https://www.pinterest.com/Lov3lyEllie/">
+                  <svg aria-labelledby="icon-title3">
+                    <title id="icon-title3">Pinterest</title>
+                    <use xlinkHref="#icon-950f35a71f420383dc2b54f8bf482185"></use>
+                  </svg>
+                  <span className="label">Pinterest</span>
+                </a>
+              </li>
+              <li>
+                <a className="n04" href="https://ko-fi.com/pufficats">
+                  <svg aria-labelledby="icon-title4">
+                    <title id="icon-title4">Ko-fi</title>
+                    <use xlinkHref="#icon-b4ecc41beadbec7df3b3e34a22693f76"></use>
+                  </svg>
+                  <span className="label">Ko-fi</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+            <section id="i-section">
+              <div id="container02" className="container columns">
+                <div className="wrapper">
+                  <div className="inner">
+                    <span></span>
+                    <div>
+                      <h3 id="text06">
+                        <em>GUIDE.</em>
+                      </h3>
+                      <ul id="links02" className="style2 links">
+                        <li className="n01">
+                          <a href="#i">guide.</a>
+                        </li>
+                        <li className="n02">
+                          <a href="#ii">roster.</a>
+                        </li>
+                        <li className="n03">
+                          <a href="#iii">connections.</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div id="container05" className="container columns full">
+                <div className="wrapper">
+                  <div className="inner">
+                    <div>
+                      <ul id="buttons02" className="buttons">
+                        <li>
+                          <a className="button n01">Ellie, 20. She/Her. INTJ-A. CS student. GMT+01.</a>
+                        </li>
+                      </ul>
+                      <p id="text07" className="style1">
+                        <span className="p">
+                          <strong>[&nbsp;002 &nbsp;]</strong>
+                          &nbsp;
+                          <mark>
+                            <span style={{color: "transparent"}}>l</span>
+                            likes<span style={{color: "transparent"}}>l</span>
+                          </mark>
+                          <span style={{color: "#E4F2F6"}}>fish, cats, the ocean, sunset/rise, space, stars, A.I, coding, Genshin,</span>
+                          <strong>doing my hobbies</strong>
+                          <span style={{color: "#E4F2F6"}}>positive people :C</span>
+                          <span style={{color: "#E4F2F6"}}>
+                            , dark minimalist aesthetic, futurism, <strong>horror movies/series</strong>
+                            .
+                          </span>
+                          <span style={{color: "#E4F2F6"}}>(To be honest, I find myself easily attracted to religious people //3)</span>
+                          <em>italics.</em>
+                          <strong>bold.</strong>
+                          <code>code.</code>
+                          <mark>highlight.</mark>
+                        </span>
+                        <span className="p">
+                          <strong>[&nbsp;002 &nbsp;]:</strong>
+                          &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                        </span>
+                      </p>
+                      <div id="list02" className="list">
+                        <ul>
+                          <li>
+                            <p>add bullet points if you like, just to elaborate on your rules or whatever. up to u.</p>
+                          </li>
+                          <li>
+                            <p>idk I'm just putting stuff on here to take up space and show u how things look. I'm just a kiddddd and life is a nightmare.</p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div>
+                      <p id="text08" className="style1">
+                        <span className="p">
+                          <strong>[&nbsp;003 &nbsp;]:</strong>
+                          &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                        </span>
+                        <span className="p">
+                          <strong>[&nbsp;004 &nbsp;]:</strong>
+                          &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                        </span>
+                        <span className="p">
+                          <strong>[&nbsp;005 &nbsp;]:</strong>
+                          &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                        </span>
+                      </p>
+                    </div>
+                    <div>
+                      <p id="text09" className="style1">
+                        <span className="p">
+                          <strong>[&nbsp;006 &nbsp;]:</strong>
+                          &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                        </span>
+                        <span className="p">
+                          <strong>[&nbsp;007 &nbsp;]:</strong>
+                          &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                        </span>
+                        <span className="p">
+                          <strong>[&nbsp;008 &nbsp;]:</strong>
+                          &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia?
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/Guide.js
@@ -0,0 +1,110 @@
+export function Guide() {
+    return (
+        <section id="i-section">
+            <div id="container02" className="container columns">
+                <div className="wrapper">
+                    <div className="inner">
+                        <span></span>
+                        <div>
+                            <h3 id="text06">
+                                <em>GUIDE.</em>
+                            </h3>
+                            <ul id="links02" className="style2 links">
+                                <li className="n01">
+                                    <a href="#i">guide.</a>
+                                </li>
+                                <li className="n02">
+                                    <a href="#ii">roster.</a>
+                                </li>
+                                <li className="n03">
+                                    <a href="#iii">connections.</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="container05" className="container columns full">
+                <div className="wrapper">
+                    <div className="inner">
+                        <div>
+                            <ul id="buttons02" className="buttons">
+                                <li>
+                                    <a href="#" className="button n01">Ellie, 20. She/Her. INTJ-A. CS student. GMT+01.</a>
+                                </li>
+                            </ul>
+                            <p id="text07" className="style1">
+                                <span className="p">
+                                    <strong>[&nbsp;002 &nbsp;]&nbsp;:</strong>
+                                    &nbsp;
+                                    <mark>
+                                        <span style={{color: "transparent"}}>l</span>
+                                        likes<span style={{color: "transparent"}}>l</span>
+                                    </mark>
+                                    <span style={{color: "#E4F2F6"}}>fishs, cats, the ocean, sunset/rise, space, stars, A.I, coding, Genshin,</span>
+                                    <strong>doing ma hobbies</strong>
+                                    <span style={{color: "#E4F2F6"}}>positive people :C</span>
+                                    <span style={{color: "#E4F2F6"}}>
+                                        , dark minimalist aesthetic, futurism, <strong>horror movies/series</strong>
+                                        .
+                                    </span>
+                                    <span style={{color: "#E4F2F6"}}>(honestly, I really get attracted so easily to religious people //3)</span>
+                                    <em>italics.</em>
+                                    <strong>bold.</strong>
+                                    <code>code.</code>
+                                    <mark>highlight.</mark>
+                                </span>
+                                <span className="p">
+                                    <strong>[&nbsp;002 &nbsp;]&nbsp;:</strong>
+                                    &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                                </span>
+                            </p>
+                            <div id="list02" className="list">
+                                <ul>
+                                    <li>
+                                        <p>add bullet points if you like, just to elaborate on your rules or whatever. up to u.</p>
+                                    </li>
+                                    <li>
+                                        <p>idk I &#39;m just putting stuff on here to take up space and show u how things look. I &#39;m just a kiddddd and life is a nightmare.</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div>
+                            <p id="text08" className="style1">
+                                <span className="p">
+                                    <strong>[&nbsp;003 &nbsp;]&nbsp;:</strong>
+                                    &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                                </span>
+                                <span className="p">
+                                    <strong>[&nbsp;004 &nbsp;]&nbsp;:</strong>
+                                    &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                                </span>
+                                <span className="p">
+                                    <strong>[&nbsp;005 &nbsp;]&nbsp;:</strong>
+                                    &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                                </span>
+                            </p>
+                        </div>
+                        <div>
+                            <p id="text09" className="style1">
+                                <span className="p">
+                                    <strong>[&nbsp;006 &nbsp;]&nbsp;:</strong>
+                                    &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                                </span>
+                                <span className="p">
+                                    <strong>[&nbsp;007 &nbsp;]&nbsp;:</strong>
+                                    &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium.
+                                </span>
+                                <span className="p">
+                                    <strong>[&nbsp;008 &nbsp;]&nbsp;:</strong>
+                                    &nbsp;Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit, morbo vel maleficia?
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
